70bab54aadbad7349511567e6d30f10da4c02cf5
Josef Strillinger <jstrillinger@outlook.com>
2022-11-09 22:32:39 +0100

    v 1.0

--- Pokerspiel/main.py
+++ Pokerspiel/main.py
@@ -1,4 +1,5 @@
 import random
+import time
 
 researched_precentages = [0.00015, 0.00135, 0.02, 0.14, 0.20, 0.39, 2.11, 4.75, 42.25, 50.2]
 
@@ -31,47 +32,57 @@ def generate_statistic():
     stats = {"Royal Flush":0, "Straight Flush":0, "Four of a kind":0, "Full House":0, "Flush":0, "Straight":0, "Three of a kind":0, "Two Pair":0, "One Pair":0, "High Card":0}
     return stats
 
+# Gibt Elemente zurück, die nicht zum max der jeweiligen liste gehört ==> verwendet um zweites pair zu finden
+def get_list_without_max(list):
+    another_pair = []
+    for x in list:
+        if x != max(list, key=list.count):
+            another_pair.append(x)
+    return another_pair
+    
 # Kombinationen der Hand ermitteln
 def calculate_hand_combinations(drawn_types, drawn_colors):
-    max_color = drawn_colors.count(max(drawn_colors, key=drawn_colors.count))
-    max_type = drawn_types.count(max(drawn_types, key=drawn_types.count))
-    if max_color == 5:
+    max_color_count = drawn_colors.count(max(drawn_colors, key=drawn_colors.count))
+    max_type_count = drawn_types.count(max(drawn_types, key=drawn_types.count))
+    
+    drawn_types.sort()
+    if max_color_count == 5:
         if list(range(8,13)) == drawn_types:
             statistic["Royal Flush"] += 1
-            return
+            return #Royal Flush
         drawn_types.sort()
-        if drawn_types[-1] -4 == drawn_types[0]:
+        if drawn_types[len(drawn_types)-1] - 4 == drawn_types[0]:
             statistic["Straight Flush"] += 1
-            return
+            return # Straight Flush
         statistic["Flush"] += 1
-        return
+        return # Flush
     
-    if max_type >= 2:
-        if max_type >= 3:
-            if max_type == 4:
-                statistic["Four of a kind"] += 1
-                return
-            else:
-                filtered_list = [x for x in drawn_types if x != max(drawn_types, key=drawn_types.count)]
-                if filtered_list.count(max(filtered_list, key=filtered_list.count)) == 2:
-                    statistic["Full House"] += 1
-                    return
-                statistic["Three of a kind"] += 1
-                return
-        else:
-            filtered_list = [x for x in drawn_types if x != max(drawn_types, key=drawn_types.count)]
-            if filtered_list.count(max(filtered_list, key=filtered_list.count)) == 2:
-                statistic["Two Pair"] += 1
-                return
-            statistic["One Pair"] += 1
-            return
+    if max_type_count == 4:
+        statistic["Four of a kind"] += 1
+        return # Four of a kind
+     
+    if max_type_count >= 3:
+        not_max_list = get_list_without_max(drawn_types)
+        if not_max_list.count(max(not_max_list, key=not_max_list.count)) == 2:
+            statistic["Full House"] += 1
+            return # Full House
+        statistic["Three of a kind"] += 1
+        return # Three of a kind
+    
+    if max_type_count >= 2:
+        not_max_list = get_list_without_max(drawn_types)
+        if not_max_list.count(max(not_max_list, key=not_max_list.count)) == 2:
+            statistic["Two Pair"] += 1
+            return # Two Pair
+        statistic["One Pair"] += 1
+        return # One Pair
         
     drawn_types.sort()
-    if drawn_types[-1] - 4 == drawn_types[0]:
+    if drawn_types[len(drawn_types)-1] - 4 == drawn_types[0]:
         statistic["Straight"] += 1
-        return
+        return # Straight
     statistic["High Card"] += 1
-    return
+    return # High Card
     
 # Ein Durchgang ==> Ziehen, Typ und Farbe ermitteln und Kombinationen ermitteln            
 def runthrough():
@@ -81,6 +92,7 @@ def runthrough():
     calculate_hand_combinations(types, colors)
 
 if __name__ == '__main__':
+    time_1 = time.time()
     tries = 100000
     statistic = generate_statistic()
     for i in range(tries):
@@ -94,3 +106,4 @@ if __name__ == '__main__':
     for i in statistic:
         print(i+": "+str(precentages[x])+" %"+" | "+"Researched: "+str(researched_precentages[x])+" %")
         x += 1
+    print("Execution Time: " + str(time.time() - time_1) + " s")
